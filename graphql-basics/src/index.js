49e9a30ab98b15a476290faf8d9097d57d4a6cbf
erick.augustor <erick.augustoramos@gmail.com>
2019-02-25 21:16:24 -0300

    Hello GraphQL

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,24 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+// Type definitions (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+`
+
+// Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is my First query!"
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => console.log("The server is up!"));
